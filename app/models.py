c5d5ff4200e90dfdaebbea412c33bf626c701200
Faustino Correa <faustino.correa.m@gmail.com>
2022-09-21 15:37:07 -0300

    creates models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    descriptions = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
