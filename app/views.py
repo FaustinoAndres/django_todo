372b213bbb25426084206fcbca087217ae902f6b
Faustino Correa <faustino.correa.m@gmail.com>
2022-09-21 14:59:02 -0300

    add about me page

--- app/views.py
+++ app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse('<h1>hello world</h1>')
+
+def about(request):
+    return HttpResponse('<h1>about me</h1>')
--- todo/urls.py
+++ todo/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import hello
+from app.views import hello, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', hello),
+    path('about', about)
 ]
