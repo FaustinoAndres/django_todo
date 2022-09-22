b59b2b147b2aedec19d29465f6df9528837cb1c1
Faustino Correa <faustino.correa.m@gmail.com>
2022-09-21 22:24:26 -0300

    install django extensions and setup ORGS_SLUGFIELD

--- todo/settings.py
+++ todo/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'organizations',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ORGS_SLUGFIELD = 'django_extensions.db.fields.AutoSlugField'
